3196a992a5736f16d658271771bf2d9d3a6493a6
Sergey Metlov <sergey.metlov@arctouch.com>
2020-11-10 15:51:38 +0300

    Create Tip_SC2_StaticConstructorException.cs

---
+++ Tip_SC2_StaticConstructorException.cs
@@ -0,0 +1,43 @@
+using System;
+
+public class Config
+{
+    public static bool Throw = true;
+}
+
+public class A
+{
+    static A()
+    {
+        // Tip CS2:
+        // Avoid static constructors to throw exceptions.
+        if (Config.Throw)
+            throw new InvalidOperationException();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            // Note 1:
+            // Despite static constructor throws InvalidOperationException
+            // the runtime will wrap it into TypeInitializationException.
+            var a = new A();
+        }
+        catch (TypeInitializationException)
+        {
+            // caught
+        }
+		
+        // Note 2:
+        // The problem is even if you re-configure things so the exception
+        // shouldn't be thrown again it still will since static constructors
+        // are only called once. Thus the following code will produce the same
+        // TypeInitializationException.
+        Config.Throw = false;
+        var a1 = new A();
+    }
+}
